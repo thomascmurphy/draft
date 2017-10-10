342b442d088dda234551bcd871c19bc9db610dbb
Thomas Murphy <thomas.c.murphy@gmail.com>
2017-10-09 22:09:04 -0600

    comment sets setup

--- flask_backend/app/api/sets.py
+++ flask_backend/app/api/sets.py
@@ -4,7 +4,8 @@
 from flask import (Blueprint, render_template, current_app, request,
                    flash, url_for, redirect, session, abort, jsonify, make_response)
 #from mtgsdk import Set
-from ..models import Set
+from ..models import Set, PackCard
+from ..models.models import *
 
 sets = Blueprint('sets', __name__, url_prefix='/api/v1/sets')
 
@@ -21,12 +22,22 @@ def get_sets():
   result_json = sorted(sets, key=lambda set: set['release_date'], reverse=True)
   return jsonify({'sets': result_json}), 201
 
-@sets.route('/seed', methods=['GET'])
-def seed_sets():
-  sets = Set.seed_data()
-  return jsonify({'sets': sets}), 201
-
-@sets.route('/<set_code>/booster', methods=['GET'])
-def booster(set_code):
-  cards = Set.generate_booster(set_code)
-  return jsonify({'cards': cards}), 201
+# @sets.route('/seed', methods=['GET'])
+# def seed_sets():
+#   sets = Set.seed_data()
+#   return jsonify({'sets': sets}), 201
+#
+# @sets.route('/<set_code>/booster', methods=['GET'])
+# def booster(set_code):
+#   cards = Set.generate_booster(set_code)
+#   return jsonify({'cards': cards}), 201
+#
+# @sets.route('/transfer_pack_cards')
+# def transfer_pack_cards():
+#   old_pack_cards = select_items('pack_cards_temp', [])
+#   for old_pack_card in old_pack_cards:
+#     old_card = select_first_item('cards_temp', ["id='%s'" % old_pack_card['card_id']])
+#     new_card = select_first_item('cards', ["multiverse_id=%i" % int(old_card['multiverse_id'])])
+#     updates = ["card_id=%i" % new_card['id'], "deck_id=%i" % int(old_pack_card['deck_id'])] if old_pack_card['deck_id'] else ["card_id=%i" % new_card['id']]
+#     new_pack_card = PackCard.update_pack_card_by_id(int(old_pack_card['id']), updates)
+#   return jsonify({'cards': cards}), 201
